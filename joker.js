6197a06846a308f5bb0ee5815fa6a4e402cfd1d4
Kesleroman <slonsky.roman@gmail.com>
2019-02-16 01:12:30 +0100

    Refactoring, added UI.

--- joker.js
+++ joker.js
@@ -1,4 +1,3 @@
-const http = require('http');
 const https = require('https')
 const fs = require ('fs')
 const nodemailer = require('nodemailer');
@@ -11,26 +10,7 @@ var transporter = nodemailer.createTransport({
     user: myMail,
     pass: ''
   }
-});
-
-const options = {
-  hostname: 'api.chucknorris.io',
-  port: 443,
-  path: '/jokes/random',
-  method: 'GET'
-}
-
-//Streams
-const Stream = require('stream')
-const readStream = new Stream.Readable()
-const writeStream = new Stream.Writable()
-
-readStream._read = () => {} // _read() must be implemented
-
-writeStream._write = (chunk, encoding, next) => {
-    console.log(chunk.toString())
-    next()
-}
+})
 
 // module for input/output
 const readline = require('readline').createInterface({
@@ -38,14 +18,19 @@ const readline = require('readline').createInterface({
   output: process.stdout
 })
 
-//const Chuck = 'https://api.chucknorris.io/jokes/random'
+const jokeSource = {
+  hostname: 'api.chucknorris.io',
+  port: 443,
+  path: '/jokes/random',
+  method: 'GET'
+}
+
 function getTheJoke() {
-  const req = https.request(options, (res) => {
+  const req = https.request(jokeSource, (res) => {
     res.on('data', (d) => {
       fs.writeFile('/tmp/joke.json', d, (err) => {
-        if(err) { console.error(err); return; };
+        if(err) { console.error(err); return }
       })
-      //process.stdout.write(d)
     })
   })
 
@@ -60,8 +45,8 @@ function chuckSaysJoke(){
   getTheJoke()
 
   const data = fs.readFileSync('/tmp/joke.json')
-  obj = JSON.parse(data)
-  joke = obj.value
+  const obj = JSON.parse(data)
+  const joke = obj.value
 
   return joke
 }
@@ -73,7 +58,7 @@ function sendEmail(data){
     to: myMail,
     subject: 'Sending Email using Node.js',
     text: data
-  };
+  }
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
@@ -81,30 +66,33 @@ function sendEmail(data){
     } else {
       console.log('Email sent: ' + info.response);
     }
-  });
+  })
 }
 
-const hostname = '127.0.0.1';
-const port = 3000;
+function start(){
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  //res.end('Hello World\n');
-});
+  console.log('Hello. This app allows you to get jokes exactly from Chuck Norris and' +
+              'send jokes to your friends\' emails. Type \'help\' to see commands.')
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-
-  //readline.question(`What's your name?`, asynch (name) => {
-    //console.log(name)
-
-    //readline.close(name)
-  //})
-
-  joke = chuckSaysJoke()
-  console.log(joke)
+  readline.on('line', (line) => {
+    switch(line){
+      case 'help':
+        console.log('joke - Chuck generates a joke and says it to standard output.'+
+                    'exit - stops the program.')
+        break
+      case 'joke':
+         joke = chuckSaysJoke()
+         console.log(joke)
+         break
+      case 'exit':
+        exit()
+      default:
+        console.log('%s is an unknown command.', line)
+    }
+  });
 
   //sendEmail(joke)
+  //process.exit()
+}
 
-});
+start()
